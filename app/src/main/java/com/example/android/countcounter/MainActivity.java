e4a7a37b2b303a176f380156ac4f40479470dd1d
Roy Mustang <roy.mustang.net@gmail.com>
2018-04-14 22:47:22 +0800

    add score function to teamB

--- app/src/main/java/com/example/android/countcounter/MainActivity.java
+++ app/src/main/java/com/example/android/countcounter/MainActivity.java
@@ -2,12 +2,59 @@ package com.example.android.countcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void Add_3_Points_for_TeamA(View view) {
+        scoreTeamA += 3;
+        displayForTeamA(scoreTeamA);
+    }
+    public void Add_2_Points_for_TeamA(View view) {
+        scoreTeamA += 2;
+        displayForTeamA(scoreTeamA);
+    }
+    public void Add_1_Points_for_TeamA(View view) {
+        scoreTeamA ++;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void Add_3_Points_for_TeamB(View view) {
+        scoreTeamB += 3;
+        displayForTeamB(scoreTeamB);
+    }
+    public void Add_2_Points_for_TeamB(View view) {
+        scoreTeamB += 2;
+        displayForTeamB(scoreTeamB);
+    }
+    public void Add_1_Points_for_TeamB(View view) {
+        scoreTeamB ++;
+        displayForTeamB(scoreTeamB);
+    }
+
 }
